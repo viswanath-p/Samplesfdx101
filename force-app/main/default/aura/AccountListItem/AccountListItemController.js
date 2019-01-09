a26407c98261e20908d4a07588522758a1500ee4
Vishwa <viswanath.p@mail.com>
2019-01-09 00:20:12 -0500

    Added Account Map Component

--- force-app/main/default/aura/AccountListItem/AccountListItemController.js
+++ force-app/main/default/aura/AccountListItem/AccountListItemController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doInit : function(component, event) {
+        var action = component.get("c.findAll");
+        action.setCallback(this, function(a) {
+          component.set("v.accounts", a.getReturnValue()); 
+        });
+        $A.enqueueAction(action);
     }
 })
